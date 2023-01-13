dcc0dcf5beb3c8b1b7cdfea291cd33874184637b
francisca hernandez <francisca.hernandez17@gmail.com>
2023-01-12 20:45:22 -0800

    ready for deployment

---
+++ src/pages/Projects/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ProjectCards from '../../components/ProjectCards';
+// import Navbar from '../../components/Navbar';
+// inport Footer from './components/Footer';
+
+
+
+const Projects = () => {
+
+    return (
+
+        <main>
+            <section>
+                {/* <Navbar /> */}
+
+                <ProjectCards></ProjectCards>
+
+
+                <div>
+                    {/* <Footer /> */}
+                </div>
+
+
+            </section>
+        </main>
+    )
+
+};
+
+export default Projects
+
+
